b2ae0dfa91c0c1111e0b5b7d692bbbe0d4b4bb70
Elhan002 <elhanhajrovic02@gmail.com>
2025-07-15 17:45:48 +0200

    Prosiren domenski model i implementirana obrada zahteva za rezervacije (kreiranje, dobavljanje, otkazivanje).

---
+++ src/tourism-api/Domain/TourReservation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System.Xml.Linq;
+
+namespace tourism_api.Domain
+{
+    public class TourReservation
+    {
+        public int Id { get; set; }
+
+        public int TouristId { get; set; }
+        public int Guests { get; set; }
+
+        public int TourId { get; set; }
+        public Tour? Tour { get; set; }
+
+        public bool IsValid() 
+        {
+            return TouristId != null && TourId != null && Guests > 0;
+        }
+    }
+}
