10ef0796fbc8fd0a8beaf85aad066d46a15f4535
dejan.jovelic <dejan.jovelic@gmail.com>
2025-07-29 21:54:39 +0200

    prosiren domenski model i implementirano kreiranje, dobavljanje i brisanje rezervacija

---
+++ src/tourism-api/Controllers/RestaurantReservationController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using tourism_api.Domain;
+using tourism_api.Repositories;
+
+namespace tourism_api.Controllers
+{
+    [Route("api/restaurants/reservations")]
+    [ApiController]
+    public class RestaurantReservationController : ControllerBase
+    {
+        private readonly RestaurantReservationRepository _restaurantReservationRepo;
+        private readonly RestaurantRepository _restaurantRepo;
+        private readonly UserRepository _userRepository;
+
+        public RestaurantReservationController(IConfiguration configuration)
+        {
+            _restaurantReservationRepo = new RestaurantReservationRepository(configuration);
+            _restaurantRepo = new RestaurantRepository(configuration);
+            _userRepository = new UserRepository(configuration);
+        }
+
+        [HttpPost]
+        public ActionResult<RestaurantReservation> Create([FromBody] RestaurantReservation newReservation)
+        {
+            if (newReservation == null || !newReservation.isValid())
+            {
+                return BadRequest("Invalid reservation data.");
+            }
+
+            try
+            {
+                Restaurant restaurant = _restaurantRepo.GetById(newReservation.RestaurantId);
+                if (restaurant == null)
+                {
+                    return NotFound($"Restaurant with ID {newReservation.RestaurantId} not found.");
+                }
+
+                User tourist = _userRepository.GetById(newReservation.TouristId);
+                if (tourist == null)
+                {
+                    return NotFound($"Tourist with ID {newReservation.TouristId} not found.");
+                }
+
+                int reservedSeats = _restaurantReservationRepo.countReservedSeats(newReservation.RestaurantId, newReservation.MealType, newReservation.ReservationDate);
+                int availableSeats = restaurant.Capacity - reservedSeats;
+
+                if (newReservation.NumberOfGuests <= availableSeats)
+                {
+                    RestaurantReservation createdReservation = _restaurantReservationRepo.Create(newReservation, restaurant.Capacity);
+                    return Ok(createdReservation);
+                }
+                else 
+                {
+                    return BadRequest($"The number of available seats for {newReservation.MealType} on {newReservation.ReservationDate.ToString("dd.MM.yyyy.")} is {availableSeats}.");
+                }
+
+            }
+            catch (Exception)
+            {
+                return Problem("An error occurred while creating new reservation.");
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<RestaurantReservation>> GetAllTouristRestaurantsReservations([FromQuery] int touristId)
+        {
+            User tourist = _userRepository.GetById(touristId);
+            if (tourist == null)
+            {
+                return NotFound($"Tourist with ID {touristId} not found.");
+            }
+
+            try
+            {
+                List<RestaurantReservation> reservations = _restaurantReservationRepo.GetAllTouristRestaurantsReservations(touristId);
+                foreach (RestaurantReservation reservation in reservations)
+                {
+                    reservation.Restaurant = _restaurantRepo.GetById(reservation.RestaurantId);
+                }
+                return Ok(reservations);
+            }
+            catch (Exception)
+            {
+                return Problem("An error occured while fetching tourist restaurant reservations.");
+            }
+        }
+
+        [HttpDelete("{reservationId}")]
+        public ActionResult DeleteReservation(int reservationId) 
+        {
+            try
+            {
+                DateTime dateOfDeletion = DateTime.Now;
+
+                RestaurantReservation reservation = _restaurantReservationRepo.GetById(reservationId);
+                if (reservation == null)
+                {
+                    return NotFound($"Reservation with ID: {reservationId} not found.");
+                }
+
+                if (reservation.MealType.Trim().ToLower() == "breakfast")
+                {
+                    TimeSpan remainingTime = reservation.ReservationDate - dateOfDeletion;
+
+                    if (remainingTime.TotalHours < 12)
+                    {
+                        return BadRequest("Breakfast cannot be cancelled less than 4 hours before the scheduled time.");
+                    }
+                    
+                }
+                else if (reservation.MealType.Trim().ToLower() == "lunch" || reservation.MealType.Trim().ToLower() == "dinner")
+                {
+                    TimeSpan remainingTime = reservation.ReservationDate - dateOfDeletion;
+
+                    if (remainingTime.TotalHours < 4)
+                    {
+                        return BadRequest($"{reservation.MealType} cannot be cancelled less than 4 hours before the scheduled time.");
+                    }
+                }
+
+                bool deletedReservation = _restaurantReservationRepo.DeleteById(reservationId);
+                if (!deletedReservation)
+                {
+                    return NotFound($"Reservation with ID: {reservationId} not found.");
+                }
+                return Ok("Reservation is cancelled.");
+
+            }
+            catch (Exception)
+            {
+                return Problem("An error occured while deleting tourist restaurant reservations.");
+            }
+        }
+    }
+}
